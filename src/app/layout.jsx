fc4e1898d8202d53e0612e07abef2cd0156f95a7
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-05 10:19:54 +0200

    start project

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
