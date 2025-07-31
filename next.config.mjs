2f5f1cd92cf9b59caf65b901f1676e932f98795c
daniil-b2be <dan.developer@born2be.media>
2025-07-31 12:26:41 +0300

    init i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost', 'holaxgroup-cms.onrender.com', 'cms.holaxgroup.com', 'res.cloudinary.com'], // Add your image domain here
-    },
-  };
+  images: {
+    domains: ['localhost', 'holaxgroup-cms.onrender.com', 'cms.holaxgroup.com', 'res.cloudinary.com'],
+  },
+};
   
-  export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -3,9 +3,11 @@ import { Inter } from "next/font/google";
 import "@/styles/base.scss";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
 
 import Preloader from "@/components/Preloader";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { routing } from "@/i18n/routing";
 /* import { PopupsProvider } from "@/context/PopupsContext"; */
 
 const inter = Inter({
@@ -25,15 +27,23 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children, params }) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>
-         <GoogleAnalytics gaId="G-XK4NN4CY2D" />
-        <Preloader />
-        <Header />
-        <main className="site">{children}</main>
-        <Footer />
+        <NextIntlClientProvider>
+          <GoogleAnalytics gaId="G-XK4NN4CY2D" />
+          <Preloader />
+          <Header />
+          <main className="site">{children}</main>
+          <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
