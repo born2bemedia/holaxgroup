ef5a9a5f5aa7ded1428b2e4a30834eaee1d98dac
Daniil <dan.developer@born2be.media>
2025-09-25 19:56:40 +0300

    [feature/products-translations]: Products Translations (#50)

--- src/app/[locale]/business-consulting/page.jsx
+++ src/app/[locale]/business-consulting/page.jsx
@@ -1,35 +1,38 @@
-import React from "react";
-import "@/styles/base.scss";
-import "@/styles/solutions.scss";
-import useProductStore from "@/stores/productsStore";
-import ProductsTabs from "./_components/ProductsTabs";
-import PackagesWrap from "./_components/PackagesWrap";
-import OrderPopup from "@/components/OrderPopup";
-import ConsultingHero from "./_components/ConsultingHero";
-import ConsultingSecond from "./_components/ConsultingSecond";
-import ConsultingPricelist from "./_components/ConsultingPricelist";
-import ConsultingWhy from "./_components/ConsultingWhy";
-import FormBottom from "../(home)/_components/FormBottom";
+import React from 'react';
+import '@/styles/base.scss';
+import '@/styles/solutions.scss';
+import useProductStore from '@/stores/productsStore';
+import ProductsTabs from './_components/ProductsTabs';
+import PackagesWrap from './_components/PackagesWrap';
+import OrderPopup from '@/components/OrderPopup';
+import ConsultingHero from './_components/ConsultingHero';
+import ConsultingSecond from './_components/ConsultingSecond';
+import ConsultingPricelist from './_components/ConsultingPricelist';
+import ConsultingWhy from './_components/ConsultingWhy';
+import FormBottom from '../(home)/_components/FormBottom';
 
 export const metadata = {
-  title: "Business Consulting Services | Holax Group",
+  title: 'Business Consulting Services | Holax Group',
   description:
-    "Elevate your business with Holax Group’ comprehensive business consulting services. From strategic planning to operational excellence, we drive growth and success.",
+    'Elevate your business with Holax Group’ comprehensive business consulting services. From strategic planning to operational excellence, we drive growth and success.',
   openGraph: {
-    title: "Business Consulting Services | Holax Group",
+    title: 'Business Consulting Services | Holax Group',
     description:
-      "Elevate your business with Holax Group’ comprehensive business consulting services. From strategic planning to operational excellence, we drive growth and success.",
+      'Elevate your business with Holax Group’ comprehensive business consulting services. From strategic planning to operational excellence, we drive growth and success.',
     //images: "https://holaxgroup.com/images/meta.png",
   },
 };
 
-const BusinessConsulting = () => {
+const BusinessConsulting = async ({ params }) => {
+  const awaitedParams = await params;
+  const { locale } = awaitedParams;
+
   return (
     <>
       <ConsultingHero />
       <ConsultingSecond />
-      <ProductsTabs />
-      <PackagesWrap />
+      <ProductsTabs locale={locale} />
+      <PackagesWrap locale={locale} />
       <ConsultingWhy />
       <ConsultingPricelist />
       <FormBottom />
