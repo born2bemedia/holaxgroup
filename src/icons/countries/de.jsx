97a3e88b3279f3e8d1e7d976709394ab42a8d76a
daniil-b2be <dan.developer@born2be.media>
2025-10-13 16:35:41 +0300

    updates

---
+++ src/icons/countries/de.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+export const De = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path d="M1.5 5.20117H22.5V9.73492H1.5V5.20117Z" fill="black" />
+    <path d="M1.5 14.2649H22.5V18.7986H1.5V14.2649Z" fill="#FFCE00" />
+    <path d="M1.5 9.73486H22.5V14.2686H1.5V9.73486Z" fill="#DD0000" />
+  </svg>
+);
